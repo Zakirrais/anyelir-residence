b23327bf5ed06fa942d4b65ef6a20dd3f9925a9c
Zakirrais <kharismudzakir1@gmail.com>
2024-12-23 23:22:34 +0700

    perubahaan index  dan css dan js

--- js/script.js
+++ js/script.js
@@ -162,3 +162,16 @@ $(Window).resize(function () {
   }
   klikMenu();
 });
+
+//efek scroll
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 0) {
+      $("nav").addClass("putih");
+    } else {
+      $("nav").removeClass("putih");
+    }
+  });
+});
