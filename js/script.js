849b893d0129bcfadf3339c465c073e7b492f8b3
Zakirrais <kharismudzakir1@gmail.com>
2024-12-23 19:05:24 +0700

    perubahaan nav dan css dan js

--- js/script.js
+++ js/script.js
@@ -132,3 +132,33 @@ function sendToWhatsApp() {
 
   window.open(url, "_blank").focus();
 }
+
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+  tombolMenu.click(function () {
+    menu.toggle();
+  });
+  menu.click(function () {
+    menu.toggle();
+  });
+}
+$(document).ready(function () {
+  var width = $(window).width();
+  if (width < 990) {
+    klikMenu();
+  }
+});
+
+//chech lebar
+$(Window).resize(function () {
+  var width = $(window).window();
+  if (width > 989) {
+    menu.css("display", "block");
+  } else {
+    menu.css("display", "none");
+  }
+  klikMenu();
+});
